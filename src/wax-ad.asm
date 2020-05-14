fe8d8a8c15979968b113a2cb2cdf677f9b8d78ab
Jason Justian <jjustian@gmail.com>
2020-05-13 21:51:56 -0400

    Reordered opcodes

--- src/wax-ad.asm
+++ src/wax-ad.asm
@@ -193,7 +193,10 @@ continue:   jsr GetOperand      ; Once $ is found, then grab the operand
             beq test
             jsr Transcribe
             jmp loop
-test:       lda #$00            ; End the buffer with a $00
+test:       lda BUFPTR          ; If the user is just hitting Return at the
+            cmp #$08            ;   prompt, just warm start without raising
+            bcc Return          ;   an error
+            lda #$00            ; End the buffer with a $00
             jsr Transcribe      ; ,,
             jsr Hypotest        ; Line is done; hypothesis test for a match
             bcc AsmFail
@@ -242,6 +245,8 @@ asm_r:      jmp Return
 ; Falls through to Return
 AsmFail     lda #"?"
             jsr CHROUT             
+            lda #$0d
+            jsr CHROUT
             
 ; Return from Wedge
 ; Return in one of two ways:
@@ -862,7 +867,71 @@ Tok2Key:    tya                 ; Save the index registers for use here
 ;     mode of the insruction, as shown in the Constants labels at the top
 ;     of the source code
 ;
-LangTable:  .byte $69,$09,$a1   ; ADC #oper
+LangTable:  .byte $ea,$07,$b7   ; NOP
+            .byte $60,$04,$ba   ; RTS
+            .byte $a9,$1b,$a0   ; LDA #oper
+            .byte $a5,$1b,$70   ; LDA oper
+            .byte $b5,$1b,$80   ; LDA oper,X
+            .byte $ad,$1b,$40   ; LDA oper
+            .byte $bd,$1b,$50   ; LDA oper,X
+            .byte $b9,$1b,$60   ; LDA oper,Y
+            .byte $a1,$1b,$20   ; LDA (oper,X)
+            .byte $b1,$1b,$30   ; LDA (oper),Y
+            .byte $4c,$1f,$47   ; JMP oper
+            .byte $6c,$1f,$17   ; JMP (oper)
+            .byte $20,$2f,$49   ; JSR oper
+            .byte $a2,$1b,$ad   ; LDX #oper
+            .byte $a6,$1b,$7d   ; LDX oper
+            .byte $b6,$1b,$9d   ; LDX oper,Y
+            .byte $ae,$1b,$4d   ; LDX oper
+            .byte $be,$1b,$6d   ; LDX oper,Y
+            .byte $a0,$1b,$ae   ; LDY #oper
+            .byte $a4,$1b,$7e   ; LDY oper
+            .byte $b4,$1b,$8e   ; LDY oper,X
+            .byte $ac,$1b,$4e   ; LDY oper
+            .byte $bc,$1b,$5e   ; LDY oper,X
+            .byte $c9,$14,$a7   ; CMP #oper
+            .byte $c5,$14,$77   ; CMP oper
+            .byte $d5,$14,$87   ; CMP oper,X
+            .byte $cd,$14,$47   ; CMP oper
+            .byte $dd,$14,$57   ; CMP oper,X
+            .byte $d9,$14,$67   ; CMP oper,Y
+            .byte $c1,$14,$27   ; CMP (oper,X)
+            .byte $d1,$14,$37   ; CMP (oper),Y
+            .byte $e0,$16,$ad   ; CPX #oper
+            .byte $e4,$16,$7d   ; CPX oper
+            .byte $ec,$16,$4d   ; CPX oper
+            .byte $c0,$16,$ae   ; CPY #oper
+            .byte $c4,$16,$7e   ; CPY oper
+            .byte $cc,$16,$4e   ; CPY oper
+            .byte $90,$19,$c1   ; BCC oper
+            .byte $b0,$19,$ca   ; BCS oper
+            .byte $f0,$28,$c8   ; BEQ oper
+            .byte $d0,$1d,$c3   ; BNE oper
+            .byte $ca,$0a,$bd   ; DEX
+            .byte $88,$0a,$be   ; DEY
+            .byte $e8,$26,$bd   ; INX
+            .byte $c8,$26,$be   ; INY
+            .byte $85,$22,$70   ; STA oper
+            .byte $95,$22,$80   ; STA oper,X
+            .byte $8d,$22,$40   ; STA oper
+            .byte $9d,$22,$50   ; STA oper,X
+            .byte $99,$22,$60   ; STA oper,Y
+            .byte $81,$22,$20   ; STA (oper,X)
+            .byte $91,$22,$30   ; STA (oper),Y
+            .byte $86,$22,$7d   ; STX oper
+            .byte $96,$22,$9d   ; STX oper,Y
+            .byte $8e,$22,$4d   ; STX oper
+            .byte $84,$22,$7e   ; STY oper
+            .byte $94,$22,$8e   ; STY oper,X
+            .byte $8c,$22,$4e   ; STY oper
+            .byte $aa,$05,$bd   ; TAX
+            .byte $a8,$05,$be   ; TAY
+            .byte $ba,$21,$bd   ; TSX
+            .byte $8a,$12,$b0   ; TXA
+            .byte $9a,$12,$ba   ; TXS
+            .byte $98,$23,$b0   ; TYA
+            .byte $69,$09,$a1   ; ADC #oper
             .byte $65,$09,$71   ; ADC oper
             .byte $75,$09,$81   ; ADC oper,X
             .byte $6d,$09,$41   ; ADC oper
@@ -883,90 +952,28 @@ LangTable:  .byte $69,$09,$a1   ; ADC #oper
             .byte $16,$00,$86   ; ASL oper,X
             .byte $0e,$00,$46   ; ASL oper
             .byte $1e,$00,$56   ; ASL oper,X
-            .byte $90,$19,$c1   ; BCC oper
-            .byte $b0,$19,$ca   ; BCS oper
-            .byte $f0,$28,$c8   ; BEQ oper
             .byte $24,$25,$7b   ; BIT oper
             .byte $2c,$25,$4b   ; BIT oper
             .byte $30,$10,$c4   ; BMI oper
-            .byte $d0,$1d,$c3   ; BNE oper
             .byte $10,$0c,$c6   ; BPL oper
             .byte $00,$2c,$b5   ; BRK
-            .byte $50,$2a,$c1   ; BVC oper
-            .byte $70,$2a,$ca   ; BVS oper
             .byte $18,$1a,$b1   ; CLC
-            .byte $d8,$1a,$b2   ; CLD
+            .byte $38,$01,$b1   ; SEC
+            .byte $78,$01,$b4   ; SEI
             .byte $58,$1a,$b4   ; CLI
-            .byte $b8,$1a,$bc   ; CLV
-            .byte $c9,$14,$a7   ; CMP #oper
-            .byte $c5,$14,$77   ; CMP oper
-            .byte $d5,$14,$87   ; CMP oper,X
-            .byte $cd,$14,$47   ; CMP oper
-            .byte $dd,$14,$57   ; CMP oper,X
-            .byte $d9,$14,$67   ; CMP oper,Y
-            .byte $c1,$14,$27   ; CMP (oper,X)
-            .byte $d1,$14,$37   ; CMP (oper),Y
-            .byte $e0,$16,$ad   ; CPX #oper
-            .byte $e4,$16,$7d   ; CPX oper
-            .byte $ec,$16,$4d   ; CPX oper
-            .byte $c0,$16,$ae   ; CPY #oper
-            .byte $c4,$16,$7e   ; CPY oper
-            .byte $cc,$16,$4e   ; CPY oper
             .byte $c6,$0a,$71   ; DEC oper
             .byte $d6,$0a,$81   ; DEC oper,X
             .byte $ce,$0a,$41   ; DEC oper
             .byte $de,$0a,$51   ; DEC oper,X
-            .byte $ca,$0a,$bd   ; DEX
-            .byte $88,$0a,$be   ; DEY
-            .byte $49,$02,$a9   ; EOR #oper
-            .byte $45,$02,$79   ; EOR oper
-            .byte $55,$02,$89   ; EOR oper,X
-            .byte $4d,$02,$49   ; EOR oper
-            .byte $5d,$02,$59   ; EOR oper,X
-            .byte $59,$02,$69   ; EOR oper,Y
-            .byte $41,$02,$29   ; EOR (oper,X)
-            .byte $51,$02,$39   ; EOR (oper),Y
             .byte $e6,$26,$71   ; INC oper
             .byte $f6,$26,$81   ; INC oper,X
             .byte $ee,$26,$41   ; INC oper
             .byte $fe,$26,$51   ; INC oper,X
-            .byte $e8,$26,$bd   ; INX
-            .byte $c8,$26,$be   ; INY
-            .byte $4c,$1f,$47   ; JMP oper
-            .byte $6c,$1f,$17   ; JMP (oper)
-            .byte $20,$2f,$49   ; JSR oper
-            .byte $a9,$1b,$a0   ; LDA #oper
-            .byte $a5,$1b,$70   ; LDA oper
-            .byte $b5,$1b,$80   ; LDA oper,X
-            .byte $ad,$1b,$40   ; LDA oper
-            .byte $bd,$1b,$50   ; LDA oper,X
-            .byte $b9,$1b,$60   ; LDA oper,Y
-            .byte $a1,$1b,$20   ; LDA (oper,X)
-            .byte $b1,$1b,$30   ; LDA (oper),Y
-            .byte $a2,$1b,$ad   ; LDX #oper
-            .byte $a6,$1b,$7d   ; LDX oper
-            .byte $b6,$1b,$9d   ; LDX oper,Y
-            .byte $ae,$1b,$4d   ; LDX oper
-            .byte $be,$1b,$6d   ; LDX oper,Y
-            .byte $a0,$1b,$ae   ; LDY #oper
-            .byte $a4,$1b,$7e   ; LDY oper
-            .byte $b4,$1b,$8e   ; LDY oper,X
-            .byte $ac,$1b,$4e   ; LDY oper
-            .byte $bc,$1b,$5e   ; LDY oper,X
             .byte $4a,$0e,$b9   ; LSR A
             .byte $46,$0e,$79   ; LSR oper
             .byte $56,$0e,$89   ; LSR oper,X
             .byte $4e,$0e,$49   ; LSR oper
             .byte $5e,$0e,$59   ; LSR oper,X
-            .byte $ea,$07,$b7   ; NOP
-            .byte $09,$03,$a0   ; ORA #oper
-            .byte $05,$03,$70   ; ORA oper
-            .byte $15,$03,$80   ; ORA oper,X
-            .byte $0d,$03,$40   ; ORA oper
-            .byte $1d,$03,$50   ; ORA oper,X
-            .byte $19,$03,$60   ; ORA oper,Y
-            .byte $01,$03,$20   ; ORA (oper,X)
-            .byte $11,$03,$30   ; ORA (oper),Y
             .byte $48,$17,$b0   ; PHA
             .byte $08,$17,$b7   ; PHP
             .byte $68,$0d,$b0   ; PLA
@@ -981,8 +988,23 @@ LangTable:  .byte $69,$09,$a1   ; ADC #oper
             .byte $76,$2d,$89   ; ROR oper,X
             .byte $6e,$2d,$49   ; ROR oper
             .byte $7e,$2d,$59   ; ROR oper,X
+            .byte $09,$03,$a0   ; ORA #oper
+            .byte $05,$03,$70   ; ORA oper
+            .byte $15,$03,$80   ; ORA oper,X
+            .byte $0d,$03,$40   ; ORA oper
+            .byte $1d,$03,$50   ; ORA oper,X
+            .byte $19,$03,$60   ; ORA oper,Y
+            .byte $01,$03,$20   ; ORA (oper,X)
+            .byte $11,$03,$30   ; ORA (oper),Y
+            .byte $49,$02,$a9   ; EOR #oper
+            .byte $45,$02,$79   ; EOR oper
+            .byte $55,$02,$89   ; EOR oper,X
+            .byte $4d,$02,$49   ; EOR oper
+            .byte $5d,$02,$59   ; EOR oper,X
+            .byte $59,$02,$69   ; EOR oper,Y
+            .byte $41,$02,$29   ; EOR (oper,X)
+            .byte $51,$02,$39   ; EOR (oper),Y
             .byte $40,$04,$b4   ; RTI
-            .byte $60,$04,$ba   ; RTS
             .byte $e9,$0f,$a1   ; SBC #oper
             .byte $e5,$0f,$71   ; SBC oper
             .byte $f5,$0f,$81   ; SBC oper,X
@@ -991,28 +1013,11 @@ LangTable:  .byte $69,$09,$a1   ; ADC #oper
             .byte $f9,$0f,$61   ; SBC oper,Y
             .byte $e1,$0f,$21   ; SBC (oper,X)
             .byte $f1,$0f,$31   ; SBC (oper),Y
-            .byte $38,$01,$b1   ; SEC
             .byte $f8,$01,$b2   ; SED
-            .byte $78,$01,$b4   ; SEI
-            .byte $85,$22,$70   ; STA oper
-            .byte $95,$22,$80   ; STA oper,X
-            .byte $8d,$22,$40   ; STA oper
-            .byte $9d,$22,$50   ; STA oper,X
-            .byte $99,$22,$60   ; STA oper,Y
-            .byte $81,$22,$20   ; STA (oper,X)
-            .byte $91,$22,$30   ; STA (oper),Y
-            .byte $86,$22,$7d   ; STX oper
-            .byte $96,$22,$9d   ; STX oper,Y
-            .byte $8e,$22,$4d   ; STX oper
-            .byte $84,$22,$7e   ; STY oper
-            .byte $94,$22,$8e   ; STY oper,X
-            .byte $8c,$22,$4e   ; STY oper
-            .byte $aa,$05,$bd   ; TAX
-            .byte $a8,$05,$be   ; TAY
-            .byte $ba,$21,$bd   ; TSX
-            .byte $8a,$12,$b0   ; TXA
-            .byte $9a,$12,$ba   ; TXS
-            .byte $98,$23,$b0   ; TYA
+            .byte $d8,$1a,$b2   ; CLD
+            .byte $b8,$1a,$bc   ; CLV
+            .byte $50,$2a,$c1   ; BVC oper
+            .byte $70,$2a,$ca   ; BVS oper
             .byte TABLE_END     ; End of 6502 table
 
 Token:      .byte $96,$03,$44,$45,$46   ; DEF
